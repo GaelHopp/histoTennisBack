a546c14c7efb6d5d7849a8b50978fa53a6f12526
Gael Hopp <galougaga@gmail.com>
2016-08-13 19:09:14 +0200

    add database class

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
+include ("./Base.php");
 require '../vendor/autoload.php';
-
 
 $app = new \Slim\Slim();
 
@@ -18,11 +18,30 @@ $app->get("/json", function () use ($app){
 	error_log("flag");
     $app->response->headers->set('Content-Type', 'application/json');
 
-    $app->response->setBody(json_encode("value"));
+    $app->response->setBody(json_encode(getAllPlayers()));
 });
 
 
 
+/*
+
+Routes for histoTennis
+
+*/
+
+function getAllPlayers(){
+	$db = getConnection();
+	$stmt = $db->query("SELECT * FROM Player");
+   return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
+
 $app->run();
+
+
+
+
+
 
 ?>
