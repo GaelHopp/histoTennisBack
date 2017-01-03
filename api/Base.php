ec6aaaf450e94c08fe48b1613d8a0219e2a5fac6
Gael Hopp <galougaga@gmail.com>
2017-01-03 18:55:02 +0100

    db info

--- api/Base.php
+++ api/Base.php
@@ -4,13 +4,13 @@
 
 function getConnection(){
 
-$servername = "localhost";
-$port = 8889;
-$username = "root";
-$password = "root";
+$servername = "db4free.net";
+$port = 3307;
+$username = "histotennis";
+$password = "133bfj57";
 
 	try {
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=HistoTennisTest", $username, $password);
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=histotennis", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $conn;
