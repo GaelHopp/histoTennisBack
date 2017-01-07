c132501b0778a654e3911b8a63045cba18e21cf9
Gael Hopp <galougaga@gmail.com>
2017-01-07 13:01:31 +0100

    change url

--- api/Base.php
+++ api/Base.php
@@ -4,13 +4,13 @@
 
 function getConnection(){
 
-$servername = "db4free.net";
+$servername = "eu-cdbr-west-01.cleardb.com";
 $port = 3307;
-$username = "histotennis";
-$password = "133bfj57";
+$username = "ba9e92470820ca";
+$password = "f4276cd2";
 
 	try {
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=histotennis", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=heroku_0e0bc9ee8b484c9", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $conn;
