d4fbf6524ed00b777a3f4d8b6737f4cd43e5c9ef
Gael Hopp <galougaga@gmail.com>
2017-01-07 20:56:29 +0100

    ok dev

--- api/Base.php
+++ api/Base.php
@@ -4,13 +4,13 @@
 
 function getConnection(){
 
-$servername = "eu-cdbr-west-01.cleardb.com";
-$port = 3307;
-$username = "ba9e92470820ca";
-$password = "f4276cd2";
+$servername = "localhost";
+$port = 8889;
+$username = "root";
+$password = "root";
 
 	try {
-    $conn = new PDO("mysql:host=$servername;dbname=heroku_0e0bc9ee8b484c9", $username, $password);
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=histoTennis", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $conn;
